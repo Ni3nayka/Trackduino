d0d1f24279b741a6b112ea6f6cde3d77ffc484aa
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2022-08-25 11:45:20 +0300

    Create pins.h

---
+++ src/pins.h
@@ -0,0 +1,120 @@
+/*
+ * Integration the Trackduino to Arduino IDE
+ * 
+ * This sketch is almost entirely a copy of the pin.h 
+ * file from the libraries of the official program
+ * 
+ * Bakay Egor, june 2022
+ * https://github.com/Ni3nayka/Trackduino
+ */
+
+
+//#include <avr/pgmspace.h>
+
+#define NUM_DIGITAL_PINS            72
+#define NUM_ANALOG_INPUTS           18
+
+#define SS    53
+#define MOSI  51
+#define MISO  50
+#define SCK   52
+
+#define SDA  20
+#define SCL  21
+#define LED_BUILTIN 22
+#define LED 22
+ 
+#define A0    54
+#define A1    55
+#define A2    56
+#define A3    57
+#define A4    58
+#define A5    59
+#define A6    60
+#define A7    61
+#define A8    62
+#define A9    63
+#define A10   64
+#define A11   65
+#define A12   66
+#define A13   67
+#define A14   68
+#define A15   69
+
+#define IN1   62
+#define IN2   63
+#define IN3   64
+#define IN4   65
+#define IN5   66
+#define IN6   67
+#define IN7   68
+#define IN8   69
+
+/*#define OUT5  22
+#define OUT4  26
+#define OUT3  24
+#define OUT2  25
+#define OUT1  38
+#define OUT6  37
+#define OUT7  29
+#define OUT8  28*/
+
+#define OUT1  6
+#define OUT2  7
+#define OUT3  8
+#define OUT4  4
+#define OUT5  13
+#define OUT6  37
+#define OUT7  29
+#define OUT8  28
+
+#define BTN_DOWN    39
+#define BTN_RIGHT   41
+#define BTN_LEFT    60
+#define BTN_UP      40
+#define BTN_CENTER  61
+
+#define STBY1       10
+#define STBY2       9
+#define STBY3       11
+#define STBY4       12
+
+#define MOTOR1_IN1  49
+#define MOTOR1_IN2  48
+#define MOTOR1_EN   45
+
+#define MOTOR2_IN1  47
+#define MOTOR2_IN2  43
+#define MOTOR2_EN   44
+
+#define MOTOR3_IN1  36
+#define MOTOR3_IN2  35
+#define MOTOR3_EN   46
+
+#define MOTOR4_IN1  34
+#define MOTOR4_IN2  33
+#define MOTOR4_EN   23
+
+#define COUNT_MOTOR 4
+const int STBY[COUNT_MOTOR] = {STBY1,STBY2,STBY3,STBY4};
+const int MOTOR_IN1[COUNT_MOTOR] = {MOTOR1_IN1,MOTOR2_IN1,MOTOR3_IN1,MOTOR4_IN1};
+const int MOTOR_IN2[COUNT_MOTOR] = {MOTOR1_IN2,MOTOR2_IN2,MOTOR3_IN2,MOTOR4_IN2}; 
+const int MOTOR_EN[COUNT_MOTOR] = {MOTOR1_EN,MOTOR2_EN,MOTOR3_EN,MOTOR4_EN};
+
+#define RGB_RED    30
+#define RGB_GREEN  31
+#define RGB_BLUE   32
+
+#define SOUND_PIN  27
+
+#define BT_SW  70
+#define SD_CS  53
+
+#define SERIAL_PORT_MONITOR         Serial
+#define SERIAL_PORT_HARDWARE        Serial
+#define SERIAL_PORT_HARDWARE1       Serial1
+#define SERIAL_PORT_HARDWARE2       Serial2
+#define SERIAL_PORT_HARDWARE3       Serial3
+#define SERIAL_PORT_HARDWARE_OPEN   Serial1
+#define SERIAL_PORT_HARDWARE_OPEN1  Serial2
+#define SERIAL_PORT_HARDWARE_OPEN2  Serial3
