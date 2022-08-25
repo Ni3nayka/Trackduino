bef8ca3d4c503676f8822056e31473113e19b3cc
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2022-08-25 11:45:30 +0300

    Update Trackduino.h

--- src/Trackduino.h
+++ src/Trackduino.h
@@ -1,41 +1,11 @@
-#include <Arduino.h>
-#pragma once
-
 /*
-mode:
-0 - ERROR
-1 - DIR
-2 - DIR, DIR
-3 - SPEED, DIR
-4 - SPEED, DIR, DIR
-*/
-
-#define DIR "DIR"
-#define PWM "PWM"
-
-class motor {
-
-  public:
-
-    //#define AUTOMATIC  1
-
-    motor(String a, int a_pin, String b="", int b_pin=0,  String c="", int c_pin=0);
-    int TestMode();
-    void SetSpeed(int predel);
-
-    void run(int speed);
-    void stop();
-    void block();
-
-  private:
-
-    //void Initialize();
-    //double dispKp;
-    int _max_speed;
-    byte _mode;
-    byte _pwm_pin;
-    byte _dir_1_pin;
-    byte _dir_2_pin;
-
-
-};
+ * Integration the Trackduino to Arduino IDE:
+ * https://github.com/Ni3nayka/Trackduino
+ * 
+ * author: Egor Bakay <egor_bakay@inbox.ru>
+ * write:  august 2022
+ * modify: august 2022
+ */
+ 
+#include "Trackduino_main.h"
+TrackDuino Trackduino;
